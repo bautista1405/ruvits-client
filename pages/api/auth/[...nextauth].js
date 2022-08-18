843a78c52fa3a14059790382cdbd56ea20daff9a
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2022-08-17 22:29:36 -0300

    Minor changes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,8 +6,8 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.NEXT_PUBLIC_APP_ID,
+      clientSecret: process.env.NEXT_PUBLIC_APP_ID,
     }),
   ],
 
