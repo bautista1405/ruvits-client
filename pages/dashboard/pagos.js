0f0ebf8998d9b69cc4c2491fbf1fcaef39a234ce
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2022-08-21 20:07:12 -0300

    Update pagos.js

--- pages/dashboard/pagos.js
+++ pages/dashboard/pagos.js
@@ -110,122 +110,16 @@ const Payments = () => {
                 </Button>
             </Flex>
             )}
-        {session && token.map((token) => { 
+        
+        {/* {session && token.map((token) => { 
             return (
             token.email != session.user.email && (
-            <Flex
-                h={[null, null, "100vh"]}
-                maxW="2000px"
-                flexDir={["column", "column", "row"]}
-                overflow="hidden"
-                margin={[null, null, "100px"]}
-                shadow="base"
-                rounded={[null, "md"]}
-                borderRadius="5px"
-                boxShadow='2xl' 
-                p='6'
-                pl={[null, null, 70, 10, 5]}
-                
-            >
-                {/* Column 1 */}
-                <Flex
-                    w={["100%", "100%", "10%", "15%", "15%"]}
-                    flexDir="column"
-                    alignItems="center"
-                    padding={10}
-                    color="gray.700"
-                    pl={[null, null, 100, 100, 5]}
-                
-                >
-                <Flex
-                    flexDir="column"
-                    h={[null, null, "100vh"]}
-                    justifyContent="space-between"
-                >
-                    <Flex
-                        flexDir="column"
-                        as="nav"
-                    >
-                        
-                        <Flex
-                            flexDir={["column", "column", "column", "column", "column"]}
-                            align={["center", "center", "center", "flex-start", "flex-start"]}
-                            wrap={["wrap", "wrap", "nowrap", "nowrap", "nowrap"]}
-                            justifyContent="center"
-                        >
-                            <Flex className="sidebar-items" mr={[2, 6, 0, 0, 0]}>
-                                <Link href="/dashboard" display={["none", "none", "flex", "flex", "flex"]}>
-                                    <Icon as={FiHome} fontSize="2xl" className="active-icon" />
-                                
-                                
-                                    <Text className="active" fontSize="18px" ml={2}>Inicio</Text>
-                                </Link>
-                            </Flex>
-                            <Flex className="sidebar-items" mr={[2, 6, 0, 0, 0]} mt={6}>
-                                <Link href="/dashboard/pagos" display={["none", "none", "flex", "flex", "flex"]}>
-                                    <Icon as={FiDollarSign} fontSize="2xl" />
-                                
-                                
-                                    <Text fontSize="18px" ml={1}>
-                                        Autorización de pagos {" "} {" "}
-                                        <Tooltip label="Para poder vender tus productos, tenés que vincular tu cuenta de MercadoPago">
-                                           <span> <Icon as={BsInfoCircle} /> </span>
-                                        </Tooltip>
-                                    </Text>
-                                </Link>
-                            </Flex>
-                            <Flex className="sidebar-items" mr={[2, 6, 0, 0, 0]} mt={6}>
-                                <Link href="/dashboard/mis-productos" display={["none", "none", "flex", "flex", "flex"]}>
-                                    <Icon as={FiBox} fontSize="2xl" />
-                                
-                                    <Text fontSize="18px" ml={2}>Mis productos</Text>
-                                </Link>
-                            </Flex>
-                            <Flex className="sidebar-items" mr={[2, 6, 0, 0, 0]} mt={6}>
-                                <Link href="/dashboard/mi-perfil" display={["none", "none", "flex", "flex", "flex"]}>
-                                    <Icon as={CgProfile} fontSize="2xl" />
-                                
-                                    <Text fontSize="18px" ml={2}>Mi perfil</Text>
-                                </Link>
-                            </Flex>
-                            <Flex className="sidebar-items" mr={[2, 6, 0, 0, 0]} mt={10}>
-                               <Link href="/nuevo-producto">
-                                 <Button colorScheme="white" variant="outline">+ Nuevo producto</Button>
-                                </Link> 
-                            </Flex>
-                        </Flex>
-                    </Flex>
-                    
-                </Flex>
-            </Flex>
-
-            <Flex 
-               display="column" 
-               justifyContent="center" 
-               w={["100%", "100%", "100%", "100%", "100%"]}
-               mt={10}
-               pl={[0, 100, 100]} 
-               rounded={[null, "md"]}
-               borderRadius="5px"
-               boxShadow='sm'
-            >
-              <p>
-                Para poder vender tus productos, es necesario que vincules tu cuenta de Mercado Pago con la plataforma, así podés recibir pagos.
-                
-              </p>
-              <a href={process.env.NEXT_PUBLIC_MP_API_AUTH} >
-                  <Button colorScheme='cyan' variant='solid' mt={10} color="white" >
-                      Vincular mi cuenta de Mercado Pago    
-                  </Button> 
-              </a>
-            </Flex>
             
-            </Flex>
-        ))})}
+        ))})} */}
 
         {session && token.map((token) => { 
             return (
-            token.email === session.user.email && (
+            token.email === session.user.email ? (
           <Flex
                 h={[null, null, "100vh"]}
                 maxW="2000px"
@@ -334,7 +228,120 @@ const Payments = () => {
           </Flex>
           
           </Flex>
-        ))})}
+        ) : (
+
+            <Flex
+                h={[null, null, "100vh"]}
+                maxW="2000px"
+                flexDir={["column", "column", "row"]}
+                overflow="hidden"
+                margin={[null, null, "100px"]}
+                shadow="base"
+                rounded={[null, "md"]}
+                borderRadius="5px"
+                boxShadow='2xl' 
+                p='6'
+                pl={[null, null, 70, 10, 5]}
+                
+            >
+                {/* Column 1 */}
+                <Flex
+                    w={["100%", "100%", "10%", "15%", "15%"]}
+                    flexDir="column"
+                    alignItems="center"
+                    padding={10}
+                    color="gray.700"
+                    pl={[null, null, 100, 100, 5]}
+                
+                >
+                <Flex
+                    flexDir="column"
+                    h={[null, null, "100vh"]}
+                    justifyContent="space-between"
+                >
+                    <Flex
+                        flexDir="column"
+                        as="nav"
+                    >
+                        
+                        <Flex
+                            flexDir={["column", "column", "column", "column", "column"]}
+                            align={["center", "center", "center", "flex-start", "flex-start"]}
+                            wrap={["wrap", "wrap", "nowrap", "nowrap", "nowrap"]}
+                            justifyContent="center"
+                        >
+                            <Flex className="sidebar-items" mr={[2, 6, 0, 0, 0]}>
+                                <Link href="/dashboard" display={["none", "none", "flex", "flex", "flex"]}>
+                                    <Icon as={FiHome} fontSize="2xl" className="active-icon" />
+                                
+                                
+                                    <Text className="active" fontSize="18px" ml={2}>Inicio</Text>
+                                </Link>
+                            </Flex>
+                            <Flex className="sidebar-items" mr={[2, 6, 0, 0, 0]} mt={6}>
+                                <Link href="/dashboard/pagos" display={["none", "none", "flex", "flex", "flex"]}>
+                                    <Icon as={FiDollarSign} fontSize="2xl" />
+                                
+                                
+                                    <Text fontSize="18px" ml={1}>
+                                        Autorización de pagos {" "} {" "}
+                                        <Tooltip label="Para poder vender tus productos, tenés que vincular tu cuenta de MercadoPago">
+                                           <span> <Icon as={BsInfoCircle} /> </span>
+                                        </Tooltip>
+                                    </Text>
+                                </Link>
+                            </Flex>
+                            <Flex className="sidebar-items" mr={[2, 6, 0, 0, 0]} mt={6}>
+                                <Link href="/dashboard/mis-productos" display={["none", "none", "flex", "flex", "flex"]}>
+                                    <Icon as={FiBox} fontSize="2xl" />
+                                
+                                    <Text fontSize="18px" ml={2}>Mis productos</Text>
+                                </Link>
+                            </Flex>
+                            <Flex className="sidebar-items" mr={[2, 6, 0, 0, 0]} mt={6}>
+                                <Link href="/dashboard/mi-perfil" display={["none", "none", "flex", "flex", "flex"]}>
+                                    <Icon as={CgProfile} fontSize="2xl" />
+                                
+                                    <Text fontSize="18px" ml={2}>Mi perfil</Text>
+                                </Link>
+                            </Flex>
+                            <Flex className="sidebar-items" mr={[2, 6, 0, 0, 0]} mt={10}>
+                               <Link href="/nuevo-producto">
+                                 <Button colorScheme="white" variant="outline">+ Nuevo producto</Button>
+                                </Link> 
+                            </Flex>
+                        </Flex>
+                    </Flex>
+                    
+                </Flex>
+            </Flex>
+
+            <Flex 
+               display="column" 
+               justifyContent="center" 
+               w={["100%", "100%", "100%", "100%", "100%"]}
+               mt={10}
+               pl={[0, 100, 100]} 
+               rounded={[null, "md"]}
+               borderRadius="5px"
+               boxShadow='sm'
+            >
+              <p>
+                Para poder vender tus productos, es necesario que vincules tu cuenta de Mercado Pago con la plataforma, así podés recibir pagos.
+                
+              </p>
+              <a href={process.env.NEXT_PUBLIC_MP_API_AUTH} >
+                  <Button colorScheme='cyan' variant='solid' mt={10} color="white" >
+                      Vincular mi cuenta de Mercado Pago    
+                  </Button> 
+              </a>
+            </Flex>
+            
+            </Flex>
+
+        )
+        
+        )})}
     </Box>
 
     </div>
