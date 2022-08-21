b86b340f65fbc298262ea9b4f2468df71d351821
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2022-08-21 16:40:56 -0300

    Update pagos.js

--- pages/dashboard/pagos.js
+++ pages/dashboard/pagos.js
@@ -88,7 +88,7 @@ const Payments = () => {
     
     }
 
-    console.log(session.user.mpAccessToken)
+    console.log(session.mpAccessToken)
   
   return (
     <div>
@@ -111,7 +111,7 @@ const Payments = () => {
                 </Button>
             </Flex>
             )}
-            {session && !session.user.mpAccessToken && (
+            {session && !session.mpAccessToken && (
             <Flex
                 h={[null, null, "100vh"]}
                 maxW="2000px"
@@ -222,7 +222,7 @@ const Payments = () => {
             </Flex>
         )}
 
-        {session && session.user.mpAccessToken && (
+        {session && session.mpAccessToken && (
           <Flex
                 h={[null, null, "100vh"]}
                 maxW="2000px"
