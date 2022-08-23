c24969a5551dd87dd31a41857c3cb60869d6870f
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2022-08-22 23:46:35 -0300

    Update [productId].js

--- pages/productos/[productId].js
+++ pages/productos/[productId].js
@@ -438,7 +438,7 @@ const ProductDetails = ({ product }) => {
 // It may be called again, on a serverless function, if
 // the path has not been generated.
 export async function getStaticPaths() {
-  const res = await fetch('/api/getproducts')
+  const res = await fetch('https://ruvits.com/api/getproducts')
   const products = await res.json()
 
   // Get the paths we want to pre-render based on products
@@ -457,7 +457,7 @@ export async function getStaticPaths() {
 // It may be called again, on a serverless function, if
 // revalidation is enabled and a new request comes in
 export async function getStaticProps({params}) {
-  const res = await fetch('/api/getproducts')
+  const res = await fetch('https://ruvits.com/api/getproducts')
   const products = await res.json();
   const product = products.filter(product => product.title === params.productId)
   
