6696559be94d45e3c110c2121f41ba970e487ed3
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2022-08-24 00:33:50 -0300

    Update [productId].js

--- pages/productos/[productId].js
+++ pages/productos/[productId].js
@@ -46,76 +46,99 @@ const ProductDetails = ({ product }) => {
     
   
   
-  const handleSubmit = async (req, res, e) => {
+
     
-    e.preventDefault();
     const appID = process.env.APP_ID
     const URL = `https://api.mercadopago.com/checkout/preferences?access_token=${product.mpAccessToken}` //url with the vendor's token
     
-    product.map(product => {
-      const data = {
-        "items": [
-          {
-            "title": product.title,
-            "quantity": 1,
-            "unit_price": product.price,
-            "picture_url": product.content[0]
-          }
-        ],
-        "marketplace": appID,
-        "marketplace_fee": 1,
-        "auto_return": "approved",
-        "back_urls": {
-          "success": "/successful",
-        },
-        "payment_methods": {
-          "excluded_payment_methods": [
-            {
-              "id": "atm"
-            }
-          ],
-          "excluded_payment_types": [
-            {
-              "id": "ticket"
-            }
-          ],
-        },
-      };
-      
-      
-      
-      const headers = { 
-        "Authorization": process.env.PROD_TOKEN,
-        "Content-Type": "application/json",
-        'Accept': 'application/json',
-        "Access-Control-Allow-Origin": "*",
-        'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
-      };
-      
-      axios.post(URL, data, {headers} )
-        .then(response => {
-          
-          if(typeof window !== 'undefined') {
-        
-            const testProduct = localStorage.setItem('product', JSON.stringify({
-              id: product._id,
-              title: product.title,
-              vendor: product.vendor,
-              description: product.description,
-              price: product.price,
-              content: product.content,
-            }))
-            
-          }
-          window.location.href = response.data.init_point
-          
-        
-        })
-        
-    })
     
-  };
+    product.map(product => {
+
+      if(typeof window !== 'undefined') {
+        const data = {
+          "items": [
+            {
+              "title": product.title,
+              "quantity": 1,
+              "unit_price": product.price,
+              "picture_url": product.content[0]
+            }
+          ],
+          "marketplace": appID,
+          "marketplace_fee": 1,
+          "auto_return": "approved",
+          "back_urls": {
+            "success": "/successful",
+          },
+          "payment_methods": {
+            "excluded_payment_methods": [
+              {
+                "id": "atm"
+              }
+            ],
+            "excluded_payment_types": [
+              {
+                "id": "ticket"
+              }
+            ],
+          },
+        };
+      
+        const productData = localStorage.setItem('data', data);
+      }
+    })
+
+    const data = localStorage.getItem('data');
+
+    const headers = { 
+      "Authorization": process.env.PROD_TOKEN,
+      "Content-Type": "application/json",
+      'Accept': 'application/json',
+      "Access-Control-Allow-Origin": "*",
+      'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+    };
+
+
+    const formik =  useFormik({
+      
+        onSubmit: () => {
+          try {
+                  axios.post(URL, data, {headers} )
+                  .then(response => {
+                  
+                  if(typeof window !== 'undefined') {
+                  
+                      const testProduct = localStorage.setItem('product', JSON.stringify({
+                      id: product._id,
+                      title: product.title,
+                      vendor: product.vendor,
+                      description: product.description,
+                      price: product.price,
+                      content: product.content,
+                      }))
+                      
+                  }
+                  window.location.href = response.data.init_point
+                  
+                  
+                  })
+              
+              } catch(res) {
+                  if(res.status === 500) {
+                
+                swal({
+                  title: "Oopss. Parece que hubo un error.",
+                  text: "Intenta de nuevo.",
+                  icon: "error",
+                }).then(() => {router.push('/dashboard')})
+              }
+            }    
+        },
+      });
+
+        
+    
 
   const deleteItem = (req, res) => {
     product.map(product => {
@@ -178,14 +201,6 @@ const ProductDetails = ({ product }) => {
         }).then(() => {router.push('/dashboard')})
     })
   }
-
-  const formik = useFormik({
-    initialValues: {
-      title: '',
-      description: '',
-      price: '',
-    },
-  });
 
 
   return (
@@ -281,7 +296,7 @@ const ProductDetails = ({ product }) => {
                         
                         
                         
-                          <form className="my-3" id="form-container" >
+                          <form className="my-3" id="form-container" onSubmit={formik.handleSubmit}>
               
                             <div className="my-2 inputs_login d-flex">
                             
@@ -291,7 +306,6 @@ const ProductDetails = ({ product }) => {
                                       colorScheme='teal' 
                                       variant='solid' 
                                       type="submit"
-                                      onSubmit={handleSubmit}
                                       marginLeft={5}
                                     >Comprar</Button>
                                   )
