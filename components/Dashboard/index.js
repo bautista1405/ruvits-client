f8e1a2e3507dacd7209f7fa08e4ef22e6efe5392
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2022-08-28 16:03:59 -0300

    Update index.js

--- components/Dashboard/index.js
+++ components/Dashboard/index.js
@@ -130,13 +130,13 @@ export default function Dashboard({data}) {
                           <Flex className="sidebar-items" mr={[2, 6, 0, 0, 0]}>
                               <Icon as={FiHome} fontSize="2xl" className="active-icon" />
                               <Link display={["none", "none", "flex", "flex", "flex"]} href='/dashboard' _hover={{ textDecor: 'none' }}>
-                                  <Text className="active" fontSize="18px" ml={2}>Inicio</Text>
+                                  <Text className="active" fontSize="18px" ml={2} style={{cursor: 'pointer'}}>Inicio</Text>
                               </Link>
                           </Flex>
                           <Flex className="sidebar-items" mr={[2, 6, 0, 0, 0]} mt={6}>
                             <Icon as={FiDollarSign} fontSize="2xl" className="active-icon" />
                             <Link href='/dashboard/pagos' _hover={{ textDecor: 'none' }} display={["flex", "flex", "none", "flex", "flex"]}>
-                                <Text fontSize="18px" ml={1}>
+                                <Text fontSize="18px" ml={1} style={{cursor: 'pointer'}}>
                                         Autorización de pagos {" "} {" "}
                                     <Tooltip label="Para poder vender tus productos, tenés que vincular tu cuenta de MercadoPago">
                                            <span> <Icon as={BsInfoCircle} /> </span>
@@ -148,14 +148,14 @@ export default function Dashboard({data}) {
                                   <Icon as={FiBox} fontSize="2xl" />
                               <Link href="/dashboard/mis-productos" _hover={{ textDecor: 'none' }} display={["flex", "flex", "none", "flex", "flex"]}>
                               
-                                  <Text fontSize="18px" ml={2}>Mis productos</Text>
+                                  <Text fontSize="18px" ml={2} style={{cursor: 'pointer'}}>Mis productos</Text>
                               </Link>
                           </Flex>
                           <Flex className="sidebar-items" mr={[2, 6, 0, 0, 0]} mt={6}>
                                   <Icon as={CgProfile} fontSize="2xl" />
                               <Link _hover={{ textDecor: 'none' }} display={["flex", "flex", "none", "flex", "flex"]} href="/dashboard/mi-perfil">
                              
-                                  <Text fontSize="18px" ml={2}>Mi perfil</Text>
+                                  <Text fontSize="18px" ml={2} style={{cursor: 'pointer'}}>Mi perfil</Text>
                               </Link>
                           </Flex>
                           <Flex className="sidebar-items" mr={[2, 6, 0, 0, 0]} mt={10}>
