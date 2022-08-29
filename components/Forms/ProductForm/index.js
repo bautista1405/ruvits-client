7e297ff261d2d60edd47ec49c0a9d3bb70371bbc
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2022-08-29 12:24:42 -0300

    Update index.js

--- components/Forms/ProductForm/index.js
+++ components/Forms/ProductForm/index.js
@@ -132,7 +132,7 @@ export default function ProductForm() {
             icon: "warning",
           }).then(() => {router.push('/dashboard/pagos')})
         }
-      }, (300))
+      }, (3000))
     }, [])
     
     
