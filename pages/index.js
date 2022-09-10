63fb0e175f0dc7b0c8f06f83613b388effb7f075
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2022-09-10 00:22:07 -0300

    Implemented next-seo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+import { NextSeo } from 'next-seo';
 import { signIn, signOut, useSession, getSession } from "next-auth/client";
 
 import Home from '../components/Home'
@@ -10,14 +11,14 @@ export default function HomePage() {
   return (
     <div>
       <Head>
-        <title>Ruvits</title>
-        <meta
-          name="description"
-          content="Marketplace de productos digitales"
-        />
-        <meta name="twitter:image" content="/assets/ruvitsLogo-bg.png" />
         <link rel="icon" href="/favicon-32x32.png" sizes="16x16 32x32" type="image/png"/>
       </Head>
+
+      <NextSeo 
+        title="Ruvits"
+        description="Marketplace de productos digitales."
+      />
+
       <Home />
     </div>
   );
