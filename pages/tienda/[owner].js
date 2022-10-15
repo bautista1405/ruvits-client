cc9b9b5300cbc38e04a4ccb1496f6c764ff9733a
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2022-10-14 21:39:35 -0300

    Update [owner].js

--- pages/tienda/[owner].js
+++ pages/tienda/[owner].js
@@ -7,15 +7,13 @@ const StoreOwner = ({ user }) => {
 
   return (
     <>
-        {/* <Flex justify="center">
+        <Flex justify="center">
             {user.length > 0 && user.map((user) => {
                 return (
                     <Text key={user._id} >{user.name}</Text>
                 )
             }) }
-        </Flex> */}
-
-        <Text>hola</Text>
+        </Flex>
     </>
   )
 }
@@ -25,40 +23,40 @@ const StoreOwner = ({ user }) => {
 // This function gets called at build time on server-side.
 // It may be called again, on a serverless function, if
 // the path has not been generated.
-// export async function getStaticPaths() {
-//     const res = await fetch('https://ruvits.com/api/getusers')
-//     const users = await res?.data?.getUsers
+export async function getStaticPaths() {
+    const res = await fetch('https://ruvits.com/api/getusers')
+    const users = await res?.data?.getUsers || []
   
-//     // Get the paths we want to pre-render based on users
-//     const paths = users.map((user) => ({
-//       params: { userId: user._id.toString() },
+    // Get the paths we want to pre-render based on users
+    const paths = users.map((user) => ({
+      params: { userId: user._id.toString() },
       
-//     }))
+    }))
   
-//     // We'll pre-render only these paths at build time.
-//     // { fallback: blocking } will server-render pages
-//     // on-demand if the path doesn't exist.
+    // We'll pre-render only these paths at build time.
+    // { fallback: blocking } will server-render pages
+    // on-demand if the path doesn't exist.
     
-//     return { paths, fallback: 'blocking' }
-// }
-//   // This function gets called at build time on server-side.
-//   // It may be called again, on a serverless function, if
-//   // revalidation is enabled and a new request comes in
-// export async function getStaticProps({params}) {
-//     const res = await fetch('https://ruvits.com/api/getusers')
-//     const users = await res?.data?.getUsers
-//     const user = users.filter(user => user.name === params.userId)
+    return { paths, fallback: 'blocking' }
+}
+  // This function gets called at build time on server-side.
+  // It may be called again, on a serverless function, if
+  // revalidation is enabled and a new request comes in
+export async function getStaticProps({params}) {
+    const res = await fetch('https://ruvits.com/api/getusers')
+    const users = await res?.data?.getUsers || []
+    const user = users.filter(user => user.name === params.userId)
     
-//     return {
-//       props: {
-//         user
-//       },
-//       // Next.js will attempt to re-generate the page:
-//       // - When a request comes in
-//       // - At most once every 10 seconds
-//       revalidate: 10, // In seconds
-//     }
-// }
+    return {
+      props: {
+        user
+      },
+      // Next.js will attempt to re-generate the page:
+      // - When a request comes in
+      // - At most once every 10 seconds
+      revalidate: 10, // In seconds
+    }
+}
   
 
 export default StoreOwner
