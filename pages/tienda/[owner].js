8bbb535f1f3ba87f7f8e2cc551231343ff076eef
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2022-11-16 19:05:49 -0300

    Update [owner].js

--- pages/tienda/[owner].js
+++ pages/tienda/[owner].js
@@ -61,15 +61,11 @@ const StoreOwner = ({ user, rating }) => {
         
     }, [getStores, getStoreProducts, getSales, getRating]) 
 
-    const store = stores.filter(store => store.email === user[0].email )
-    const storeProducts = products.filter(storeProducts => storeProducts.vendor === user[0].name)
-    const storeSales = sales.filter(sale => sale.vendor === user[0].name)
-    const storeRating = ownerRating.filter(rating => rating._id === user[0].name)
-
+    
     console.log(storeRating)
-
-  return (
-    <>    
+    
+    return (
+      <>    
       <Head>
         {user.map((user) => {
           return (
@@ -81,6 +77,10 @@ const StoreOwner = ({ user, rating }) => {
             <link rel="icon" href="/favicon-32x32.png" sizes="16x16 32x32" type="image/png"/>
       </Head> 
         {user.length > 0 ? user.map((user) => {
+          const store = stores.filter(store => store.email === user[0].email )
+          const storeProducts = products.filter(storeProducts => storeProducts.vendor === user[0].name)
+          const storeSales = sales.filter(sale => sale.vendor === user[0].name)
+          const storeRating = ownerRating.filter(rating => rating._id === user[0].name)
           return (
             <Box key={user._id}>
           
