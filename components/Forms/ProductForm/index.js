f680cc3d124a4a72ec5c59339559bdbda2a5a345
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2023-01-24 18:07:14 -0300

    Update index.js

--- components/Forms/ProductForm/index.js
+++ components/Forms/ProductForm/index.js
@@ -134,7 +134,7 @@ export default function ProductForm() {
             
             <Spinner
                     thickness='4px'
-                    speed='0.65s'
+                    speed='0.66s'
                     emptyColor='gray.200'
                     color='blue.500'
                     size='xl'
